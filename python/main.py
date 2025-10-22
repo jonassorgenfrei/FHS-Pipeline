f2bd01493c660c1bcfc9919e34cdc233c3ee4058
Jonas Sorgenfrei <jonas_sorgenfrei@yahoo.de>
2025-10-22 21:16:41 +0200

    python playground file

---
+++ python/main.py
@@ -0,0 +1,28 @@
+import json
+
+
+def write_data(param:int) -> str:
+    json_data : dict = {
+        "number": param,
+    }
+    json_data["myNew_key"]= "myNew_value"
+    
+    for i in range(100):
+        json_data[f"key_{i}"] = f"value_{i}"
+    
+    with open("D:/Documents/Development/FH_Salzburg/pipeline/.data/output.json", "w") as json_file:
+        json.dump(json_data, json_file, indent=4)
+    
+    return str(json.dumps(json_data, indent=4))
+
+
+def load_data():
+    with open("D:/Documents/Development/FH_Salzburg/pipeline/.data/output.json", "r") as json_file:
+        data = json.load(json_file)
+
+    print(json.dumps(data, indent=4))
+
+
+write_data(42)
+
+load_data()
