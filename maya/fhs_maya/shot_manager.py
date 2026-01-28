7aac1c54667b9b8732371ca8b8c0ddf801d2ad94
Jonas Sorgenfrei <jonas_sorgenfrei@yahoo.de>
2026-01-28 22:06:20 +0100

    adding shot manager implementation for maya

---
+++ maya/fhs_maya/shot_manager.py
@@ -0,0 +1,60 @@
+from maya import cmds
+import os
+from fhs.shotManager.shotManager import ShotManager
+try:
+    from PySide6 import QtWidgets
+except ModuleNotFoundError:
+    from PySide2 import QtWidgets
+    
+
+class Maya_ShotManager(ShotManager):
+    def __init__(self):
+        """Initialize the Maya Shot Manager GUI."""
+        # construct parent
+        super().__init__()
+
+    def create_workfile(self, shot_name:str, shot_directory:str, start_frame:int, end_frame:int) -> str:
+        """Create a Maya .ma workfile for the shot.
+        
+        Args:
+            shot_name (str): _name of the shot
+            shot_directory (str): _path to the shot directory
+            start_frame (int): _start frame of the shot
+            end_frame (int): _end frame of the shot
+        Returns:
+            str: _path to the created .ma file
+            
+        """
+        # create ma file 
+        ma_file_path=os.path.join(shot_directory, f"{shot_name}.v001.ma")
+        
+        cmds.file(rename=ma_file_path)
+        cmds.file(save=True, type="mayaAscii")  
+        
+        self.init_scene(shot_name, start_frame, end_frame)
+        
+        # save ma file after manual changes
+        cmds.file(save=True, type="mayaAscii")
+
+        return ma_file_path
+    
+    def open_workfile(self, item: QtWidgets.QListWidgetItem) -> None:
+        """Open the selected workfile in the DCC application."""
+        workfile_path = item.text()
+        
+        if workfile_path.endswith(".ma"):
+            cmds.file(workfile_path, open=True, force=True)
+            self.close()
+        else:
+            super().open_workfile(item)
+
+    def init_scene(self, shot_name:str, start_frame:int, end_frame:int):
+        """Initialize the Maya scene for the shot.   
+        Args:
+            shot_name (str): _name of the shot
+            start_frame (int): _start frame of the shot
+            end_frame (int): _end frame of the shot
+        """
+        # set frame range
+        cmds.playbackOptions(min=start_frame, max=end_frame)
+        cmds.currentTime(start_frame)
--- maya/fhs_maya/startUp.py
+++ maya/fhs_maya/startUp.py
@@ -1,6 +1,7 @@
 from maya import cmds, utils
 from fhs.core.core import helloCore
 
+shot_manager_window = None
 
 def createSpheres(arg):
     for r in range(100):
@@ -8,6 +9,18 @@ def createSpheres(arg):
         cmds.move(r, 0, 0, sphere)
     helloCore()
 
+def open_shot_manager(arg):
+    global shot_manager_window
+    from fhs_maya.shot_manager import Maya_ShotManager
+
+    try:
+        shot_manager_window.close()
+        shot_manager_window.deleteLater()
+    except:
+        pass
+
+    shot_manager_window = Maya_ShotManager()
+    shot_manager_window.show()
 
 def createMenu():
     menu_name = "fhsMenu"
@@ -18,7 +31,8 @@ def createMenu():
 
     cmds.menu(menu_name, label=menu_label, parent="MayaWindow", tearOff=True)
     cmds.menuItem(label="Create 100 Spheres", command=createSpheres)
-    
+    cmds.menuItem(divider=True)
+    cmds.menuItem(label="Open Shot Manager", command=open_shot_manager)
     
 def startup():
     utils.executeDeferred(createMenu)
